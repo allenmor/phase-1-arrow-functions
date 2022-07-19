016907e4b915bbf29398ef86e17e629170157308
Allen Mordukhaev <allenmor93@gmail.com>
2022-07-19 09:28:24 -0400

    commit tree 152a88dd1ee6df1027e3fca064ef7c8f3be7e8c7

---
+++ index.js
@@ -0,0 +1,10 @@
+// arrow function that divides
+const divide = (a, b) => a / b;
+
+// arrow function that squares
+
+const square = (a) => a * a;
+
+// arrow function that adds
+
+const add = (a, b) => a + b;
